d952373e19f76c8b4f27197961c61b2e87baefa6
Mstrange8 <matthewsstrange@gmail.com>
2021-01-20 08:45:31 -0700

    admin exports

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,12 +10,15 @@ exports.getAdmin = (req, res, next) => {
 };
 
 exports.getBlogs = (req, res, next) => {
-    Blog.fetchAll(blogs => {
+    Blog.fetchAll().then(blogs => {
         res.render('admin/blogs', {
             pageTitle: 'Admin Blog',
             path: '/admin/blogs',
             blogs: blogs
         });
+    })
+    .catch(err => {
+        console.log(err);
     });
 };
 
@@ -32,8 +35,13 @@ exports.postAddBlog = (req, res, next) => {
     const title = req.body.title;
     const blog = req.body.blog;
     const newBlog = new Blog(order, title, blog);
-    newBlog.save();
-    res.redirect('/admin/blogs');
+    newBlog.save()
+    .then(() => {
+        res.redirect('/admin/blogs');
+    })
+    .catch(err => {
+        console.log(err);
+    });
 };
 
 exports.getEditBlog = (req, res, next) => {
@@ -42,16 +50,17 @@ exports.getEditBlog = (req, res, next) => {
         res.redirect('/');
     }
     const title = req.params.blogTitle;
-    Blog.findByTitle(title, blog => {
-        if (!blog) {
-            return res.redirect('/');
-        }
+    Blog.findByTitle(title)
+        .then(blog => {
         res.render('admin/add-blog', {
             pageTitle: 'Edit Blog',
             path: '/admin/edit-blog',
             editing: editMode,
             blog: blog
         });
+    })
+    .catch(err => {
+        console.log(err);
     });
 };
 
@@ -64,12 +73,22 @@ exports.postEditBlog = (req, res, next) => {
         title,
         blog
     );
-    updatedBlog.save();
-    res.redirect('/admin/blogs');
+    updatedBlog.save()
+    .then(() => {
+        res.redirect('/admin/blogs');
+    })
+    .catch(err => {
+        console.log(err);
+    });
 };
 
 exports.postDeleteBlog = (req, res, next) => {
     const blogTitle = req.body.blogTitle;
-    Blog.deleteByTitle(blogTitle);
-    res.redirect('/admin/blogs');
+    Blog.deleteByTitle(blogTitle)
+    .then(() => {
+        res.redirect('/admin/blogs');
+    })
+    .catch(err => {
+        console.log(err);
+    });
 };
