0c400e179b0de1bed46553a82c5242c7fd552ecb
Mstrange8 <matthewsstrange@gmail.com>
2021-01-20 08:46:42 -0700

    blog exports

--- controllers/blog.js
+++ controllers/blog.js
@@ -2,21 +2,27 @@ const Blog = require('../models/blog');
 
 exports.getBlogPage = (req, res, next) => {
     const blogTitle = req.params.blogTitle;        
-    Blog.findByTitle(blogTitle, blog => {
+    Blog.findByTitle(blogTitle).then(blog => {
         res.render('chosen-blog', {
             pageTitle: 'Blog',
             path: '/' + blogTitle,
             blog: blog
         });
+    })
+    .catch(err => {
+        console.log(err);
     });  
 };
 
 exports.getBlogs = (req, res, next) => {
-    Blog.fetchAll(blogs => {
+    Blog.fetchAll().then(blogs => {
         res.render('blog', {
             pageTitle: 'Blog',
             path: '/',
             blogs: blogs
         });
+    })
+    .catch(err => {
+        console.log(err);
     });
 };
